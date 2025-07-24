84799ad61a6a550d93370880e53c0bcfebbde7d4
rifki-iky <rifkisuandi04@gmail.com>
2025-07-24 16:54:05 +0700

    Delete scrift.js

--- scrift.js
+++
@@ -1,9 +0,0 @@
-const btnNo = document.getElementById('btnNo');
-
-btnNo.addEventListener('mouseover', function () {
-  const i = Math.floor(Math.random() * window.innerWidth - 100);
-  const j = Math.floor(Math.random() * window.innerHeight - 40);
-  btnNo.style.position = 'absolute';
-  btnNo.style.left = i + 'px';
-  btnNo.style.top = j + 'px';
-});
