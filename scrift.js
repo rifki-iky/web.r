7abd1535ed022a12a7ea8a5cf85c593a36cb9e19
rifki-iky <rifkisuandi04@gmail.com>
2025-07-25 15:58:21 +0700

    Delete scrift.js

--- scrift.js
+++
@@ -1,9 +0,0 @@
-const btnNo = document.getElementById('btnNo');
-
-btnNo.addEventListener('click', function () {
-  const randomX = Math.floor(Math.random() * (window.innerWidth - 100));
-  const randomY = Math.floor(Math.random() * (window.innerHeight - 60));
-  btnNo.style.position = 'absolute';
-  btnNo.style.left = `${randomX}px`;
-  btnNo.style.top = `${randomY}px`;
-});
